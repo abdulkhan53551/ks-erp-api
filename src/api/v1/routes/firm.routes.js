0edfe85278703f982e9df7eeb668de2a04048be1
Abdul Khan <abdulkhan5355@gmail.com>
2025-07-23 01:21:47 +0530

    Added firm pagination route

--- src/api/v1/routes/firm.routes.js
+++ src/api/v1/routes/firm.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
 const validate = require('../middlewares/validate')
 const { createFirmValidationSchema, updateFirmValidationSchema, deleteFirmValidationSchema, getFirmByIdValidationSchema } = require('../validation/firm.validation')
-const { createFirm, getAllFirm, updateFirm, deleteFirm, getFirmType, getFirmById } = require('../controllers/firm.controller')
+const { createFirm, getAllFirm, updateFirm, deleteFirm, getFirmType, getFirmById, getFirmMeta } = require('../controllers/firm.controller')
 const router = Router()
 
 // Firm
 router.get('/firm-type', getFirmType);
+router.get('/firm-pagination', getFirmMeta);
 router.get('/', getAllFirm);
 router.get('/:id', validate(getFirmByIdValidationSchema), getFirmById);
 router.post('/', validate(createFirmValidationSchema), createFirm);
